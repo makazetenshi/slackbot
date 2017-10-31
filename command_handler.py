fed4a262f99d1b8a91ade5cf39b80536c0df928b
makazetenshi <frejsps2@gmail.com>
2017-10-31 10:05:13 +0100

    add mainhandler and commandhandler

---
+++ command_handler.py
@@ -0,0 +1,28 @@
+import yaml
+import logging
+from io import open
+import message_dispatcher as dispatcher
+class CommandHandler(object):
+    FORMAT = "%(asctime)-15s %(message)s"
+    logging.basicConfig(filename='swapbot.log', level=logging.DEBUG, format=FORMAT)
+
+    @staticmethod
+    def handle(integration, command, channel, slack_client, args):
+        if integration != None:
+            integration_module = __import__(integration['module'])
+            integration_class = getattr(integration_module, integration['class'])
+            dyn_integration = integration_class()
+            message = dyn_integration.parse_command(command, channel, args)
+        else:
+            message = "Unknown integration/commmand"
+        dispatcher.MessageDispatcher().dispatch_message(channel, message, slack_client)
+
+    def get_integration(self, command, channel, slack_client, args):
+        date_string = "%Y-%m-%d %H:%M:%S"
+        with open("integrations.yaml") as stream:
+            try:
+                parsed_command = command.split()[0]
+                integration = yaml.load(stream)[parsed_command]
+                self.handle(integration, command, channel, slack_client, args)
+            except Exception as e:
+                logging.exception(e)
---
+++ mainhandler.py
@@ -0,0 +1,51 @@
+import logging
+import os
+import time
+from os.path import join, dirname
+from dotenv import load_dotenv
+import command_handler
+from slackclient import SlackClient
+class MainHandler(object):
+    global dotenv_path
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    FORMAT = "%(asctime)-15s %(message)s"
+    logging.basicConfig(filename='swapbot.log', level=logging.DEBUG, format=FORMAT)
+    global SLACK_TOKEN
+    SLACK_TOKEN = os.environ.get("SLACK_TOKEN")
+
+    @staticmethod
+    def parse_output(slack_output):
+        output_list = slack_output
+        if output_list and len(output_list) > 0:
+            for output in output_list:
+                if output and 'text' in output and (output['text'].encode('utf8')).startswith('.') and 'subtype' not in output:
+                    return output['text'][1:], \
+                            output['channel'], None
+                if output and 'file' in output and 'comment' in output:
+                    return output['comment']['comment'][1:], \
+                            output['channel'], output['file']['url_private']
+
+    def main(self):
+        date_string = "%Y-%m-%d %H:%M:%S"
+        READ_WEBSOCKET_DELAY = 1
+        global slack_client
+        slack_client = SlackClient(SLACK_TOKEN)
+        if slack_client.rtm_connect():
+            while True:
+                try:
+                    command, channel, args = self.parse_output(slack_client.rtm_read())
+                    if command and channel:
+                        command = command.encode("utf8")
+                        handler = command_handler.CommandHandler()
+                        handler.get_integration(command, channel, slack_client, args)
+                        time.sleep(READ_WEBSOCKET_DELAY)
+                except Exception as e:
+                    logging.exception(e)
+                    time.sleep(READ_WEBSOCKET_DELAY)
+                    slack_client.rtm_connect()
+        else:
+            logging.debug("Connection Failed.")
+
+if __name__ == '__main__':
+    MainHandler().main()
