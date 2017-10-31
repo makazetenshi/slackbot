b5ea75ad7d8069d2d1763db72be1a6ff17a454e5
makazetenshi <frejsps2@gmail.com>
2017-10-31 10:15:47 +0100

    add urban dictionary lookup as an additional example

---
+++ urban.py
@@ -0,0 +1,15 @@
+import json
+import requests
+class Urban:
+
+    def parse_command(self, command, channel, args):
+        parsed_command = command.split(' ', 1)
+        return self.perform_lookup(parsed_command[1:])
+
+    @staticmethod
+    def perform_lookup(query):
+        r = requests.get("http://api.urbandictionary.com/v0/define?term=" + query[0])
+        content = r.json()
+        if (content['result_type'] == 'no_results'):
+            return 'No results found matching: ' + query[0]
+        return content['list'][0]['definition']
